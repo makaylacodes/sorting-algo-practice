82323953fe098b279bc645e6276d1919cc6c3e9d
Makayla Anderson-Tucker <makayla.anderson.tucker@gmail.com>
2022-07-02 21:57:04 -0700

    Created a naive, bruteforcce sorting algorithm for arrays.

--- sorting-practice/sorting-practice.cpp
+++ sorting-practice/sorting-practice.cpp
@@ -1,20 +1,72 @@
-// sorting-practice.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// sorting-algo-practice.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
+#include <cstdlib>
+#define MAX 100
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int inputNum;
+    int arr[MAX];
+    cout << "Enter a number: ";
+    cin >> inputNum;
+    cout << endl;
+
+    /*This loop will populate the array by using the rand function*/
+    for (int i = 0; i < inputNum; i++) {
+        arr[i] = rand();
+    }
+
+
+    cout << "Unsorted array: ";
+    //prints unsorted array
+    for (int i = 0; i < inputNum; i++) {
+        cout << arr[i] << " ";
+    }
+
+    //sorting an array
+    for (int i = 0; i < inputNum; i++) {
+        for (int j = i + 1; j < inputNum; j++) {
+
+            //Ascending Order
+            if (arr[j] < arr[i]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    cout << "\n\nSorted array in ascending order: ";
+    //prints sorted array
+    for (int i = 0; i < inputNum; i++) {
+        cout << arr[i] << " ";
+    }
+
+    //sorting an array
+    for (int i = 0; i < inputNum; i++) {
+        for (int j = i + 1; j < inputNum; j++) {
+
+            //Descending Order
+            if (arr[j] > arr[i]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    cout << "\n\nSorted array in descending order: ";
+    //prints sorted array
+    for (int i = 0; i < inputNum; i++) {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl << endl;
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
